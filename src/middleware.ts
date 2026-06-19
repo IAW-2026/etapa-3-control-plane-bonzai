e9a04b2d2f8b591929ebc19ddaed0bacac6803c9
JosefinaBacci <josefinabacci015@gmail.com>
2026-06-18 23:34:26 -0300

    delete proxy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,13 @@
-export { default } from "./proxy";
-export { config } from "./proxy";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/login", "/sign-in(.*)", "/"]);
+
+export default clerkMiddleware(async (auth, req) => {
+    if (!isPublicRoute(req)) {
+        await auth.protect();
+    }
+    });
+
+    export const config = {
+    matcher: ["/((?!_next|.*\\..*).*)"],
+};
